43dbe67bc52a07e40d78d6dff384ed57aaded886
pajapolanska <paja.polanska@gmail.com>
2021-10-09 16:39:29 +0200

    1 file modified, 1 added

---
+++ objekt.js
@@ -0,0 +1,8 @@
+let osoba = {
+  jmeno: "Pavlina",
+  vek: 32,
+  povolani: "analytik",
+  svobodna: true,
+};
+
+console.log(osoba);
--- pole2.js
+++ pole2.js
@@ -6,11 +6,20 @@ while (cislo != 0) {
     pole.push(cislo)
     cislo = Number(prompt("Zadej:"));    
 }
-let nejmensiCislo = pole[0];
+/*let nejmensiCislo = pole[0];
 for(let i = 0; i < pole.length; i++) {
   if(nejmensiCislo > pole[i]) {
     nejmensiCislo = pole[i];
   }
 }
+console.log("nejmenší číslo v poli je " + nejmensiCislo); 
+*/
 
-console.log("nejmenší číslo v poli je " + nejmensiCislo);
+let nejmensi = pole[0];
+for (let cislo of pole) {
+  if (nejmensi > cislo) {
+    nejmensi = cislo;
+  }  
+}
+
+console.log("nejmenší číslo v poli je " + nejmensi);
