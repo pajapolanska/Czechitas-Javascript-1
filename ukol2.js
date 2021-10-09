865decd281a04baa1d35f862c45de601e5e01d20
pajapolanska <paja.polanska@gmail.com>
2021-10-09 14:06:40 +0200

    2 files modified

---
+++ ukol2.js
@@ -0,0 +1,8 @@
+let cislo1 = Number(prompt("Zadej první číslo"));
+let cislo2 = Number(prompt("Zadej druhé číslo"));
+
+for(let i = cislo1; i <= cislo2; i++) {
+if (i%2 === 0) {
+  console.log(i)
+  }
+}
