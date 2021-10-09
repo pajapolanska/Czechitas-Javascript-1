7cf2f27b8a37020ef665ac1f31f0a462ba3db42f
pajapolanska <paja.polanska@gmail.com>
2021-10-09 16:03:11 +0200

    2 files modified, 1 added

--- Pole.js
+++ Pole.js
@@ -1,1 +1,10 @@
-console.log("prdel");
+//indexování polí je od 0 - pozice [0, 1, 2, 3]
+let pole = [];
+console.log(pole);
+
+pole.push(31);
+console.log(pole);
+
+for( let i = 0; i < pole.length; i++);
+console.log(pole[i]);
+
